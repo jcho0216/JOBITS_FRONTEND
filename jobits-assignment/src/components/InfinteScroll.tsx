979f11c20533f09d4c3deed99f2259d72641f977
Jun Joy <chojuneseo1@gmail.com>
2021-05-30 21:33:34 +0900

    update; infinite scroll

--- jobits-assignment/src/components/InfinteScroll.tsx
+++ jobits-assignment/src/components/InfinteScroll.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { lazy, useEffect, useState } from "react";
 import * as S from "./styles";
+import InfiniteDataTypes from "../interface/InfiniteDataTypes";
 
 const InfiniteScroll = (): JSX.Element => {
-  const [data, setData] = useState<string[]>([]);
+  const [data, setData] = useState<InfiniteDataTypes[]>([]);
   let [page, setPage] = useState<number>(1);
-  const [maxPage, setMaxPage] = useState<number>(0);
+  const [maxPage, setMaxPage] = useState<InfiniteDataTypes | number>(0);
+  // const [loading, setLoading] = useState<InfiniteDataTypes | boolean>(false);
 
   window.onscroll = () => {
     const scrollHeight = document.documentElement.scrollHeight;
@@ -16,20 +18,26 @@ const InfiniteScroll = (): JSX.Element => {
       if (page > maxPage) {
         console.log("데이터가 없어요 ㅠ");
       } else {
-        setPage((page += 1));
+        setPage(page + 1);
       }
     }
   };
 
   useEffect(() => {
-    const getData = async (page: number) => {
-      const jobData = await axios.get(
-        `${process.env.REACT_APP_API_URL}/scroll?page=${page}`
-      );
-      setData([...data, ...jobData.data.data]);
-      setMaxPage(jobData.data.maxPage);
+    const getData = async (page: InfiniteDataTypes | number) => {
+      // setLoading(true)
+      await axios
+        .get(`${process.env.REACT_APP_API_URL}/scroll?page=${page}`)
+        .then((res) => {
+          setData([...data, ...res.data.data]);
+          setMaxPage(res.data.maxPage);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     };
     getData(page);
+    // setLoading(false)
   }, [page]);
 
   return (
@@ -38,7 +46,7 @@ const InfiniteScroll = (): JSX.Element => {
       <S.PostContainer>
         {data.map((data: any) => {
           return (
-            <S.Post>
+            <S.Post key={data.name}>
               <S.CompanyName>{data.name}</S.CompanyName>
               {data.createdAt}
             </S.Post>
--- jobits-assignment/src/components/styles.ts
+++ jobits-assignment/src/components/styles.ts
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
+
+export const Loading = styled.div`
+  font-size: 150px;
+  margin-left: 200px;
+  z-index: 99;
+`;
+
 //Navigation Bar styles
 export const NavBarContainer = styled.div`
-  width: 10%;
+  width: 12%;
   height: 92vh;
   padding-top: 8vh;
   background-color: #2c2c2c;
@@ -19,21 +26,18 @@ export const NavBar = styled.div`
   div {
     margin: 15px auto;
   }
-  &:hover {
-      background-color: #0f9f0e;
+  &:active {
+    background-color: #0f9f0e;
   }
 `;
 
-
 //Infinity Scroll Styles
-export const InfinityScrollContainer = styled.div`
-
-`;
+export const InfinityScrollContainer = styled.div``;
 
 export const RecruitmentRequest = styled.div`
   font-size: 20px;
-  padding: 6vh 0; 
-  margin-left: 252px;
+  padding: 6vh 0;
+  margin-left: 272px;
   font-weight: 100;
 `;
 
@@ -45,22 +49,112 @@ export const Post = styled.div`
   text-align: center;
   float: left;
   margin: 20px 30px;
-  box-shadow: 5px 5px 5px #e7e7e7e7;  
+  box-shadow: 5px 5px 5px #e7e7e7e7;
 `;
 
 export const PostContainer = styled.div`
   display: inline-block;
-  margin-left: 220px;
+  margin-left: 240px;
 `;
-
 
 export const CompanyName = styled.div`
   font-size: 25px;
-  display:flex;
+  display: flex;
   flex-direction: column;
   margin: 100px 30px 30px 30px;
 `;
 
-
-
 //Pagination Styles
+export const PageNationContainer = styled.div`
+  /* width: 100%;
+  height: 100vh;
+  background-color: #ffffff;
+  display: flex; */
+`;
+
+export const PageNation = styled.div`
+  width: 80vw;
+  height: 75vh;
+  border: 1px solid #e7e7e7;
+  position: relative;
+  left: 290px;
+  top: 50px;
+  box-shadow: 5px 5px 5px #e7e7e7;
+`;
+
+export const RecruitmentRequestPage = styled.div`
+  font-size: 20px;
+  font-weight: 100;
+  padding: 40px 80px;
+`;
+
+export const TableContainer = styled.div`
+  width: 88%;
+  height: 60vh;
+  margin-left: 80px;
+  display: inline-block;
+`;
+
+export const TableBar = styled.div`
+  display: flex;
+  border-top: 3px solid #c1c1c1;
+  margin-left: 80px;
+  width: 88%;
+  height: 70px;
+  justify-content: space-between;
+  font-size: 20px;
+  font-weight: 500;
+  span {
+    margin: auto 80px;
+    /* justify-content: space-between; */
+  }
+  
+`;
+
+export const Table = styled.li`
+  display: flex;
+  list-style: none;
+  font-size: 15px;
+  font-weight: 300;
+  width: 100%;
+  height: 70px;
+  border-top: 1px solid #c1c1c1;
+  justify-content: space-between;
+  &:nth-child(odd) {
+    background-color: #fbfbfb;
+  }
+  span {
+    margin: auto 85px;
+  }
+  
+`;
+
+export const PageContainer = styled.div`
+  display: flex;
+  margin: 80px 295px;
+`;
+
+export const Page = styled.li`
+  list-style: none; 
+  height: 25px;
+  width: 30px;
+  cursor: pointer;
+  border: 1px solid #e1e1e1;
+  border-right: none;
+  text-align: center;
+  padding-top: 5px;
+  &:active {
+    background-color: #009d00;
+  }
+`;
+
+export const Button = styled.div`
+  width: 30px;
+  height: 25px;
+  cursor: pointer;
+  text-align: center;
+  border: 1px solid #e1e1e1;
+  border-radius: 10%;
+  padding-top: 5px;
+`;
+
